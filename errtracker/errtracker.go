147d4153c12704dca0a8a9874a5e19a710c718d8
Michael Vogt <michael.vogt@ubuntu.com>
2017-02-17 12:21:42 +0100

    Import patches-unapplied version 2.23 to ubuntu/xenial-proposed
    Imported using git-ubuntu import.
    
    Changelog parent: fdef87ca1b4bc27125751b494f44494fabb92c26

--- errtracker/errtracker.go
+++ errtracker/errtracker.go
@@ -58,7 +58,7 @@ func distroRelease() string {
 	return fmt.Sprintf("%s %s", ID, release.ReleaseInfo.VersionID)
 }
 
-func Report(snap, channel, errMsg string, extra map[string]string) (string, error) {
+func Report(snap, errMsg, dupSig string, extra map[string]string) (string, error) {
 	if CrashDbURLBase == "" {
 		return "", nil
 	}
@@ -72,7 +72,7 @@ func Report(snap, channel, errMsg string, extra map[string]string) (string, erro
 
 	crashDbUrl := fmt.Sprintf("%s/%s", CrashDbURLBase, identifier)
 
-	hostSnapdPath := filepath.Join(dirs.LibExecDir, "snapd")
+	hostSnapdPath := filepath.Join(dirs.DistroLibExecDir, "snapd")
 	coreSnapdPath := filepath.Join(dirs.SnapMountDir, "core/current/usr/lib/snapd/snapd")
 	if mockedHostSnapd != "" {
 		hostSnapdPath = mockedHostSnapd
@@ -98,10 +98,9 @@ func Report(snap, channel, errMsg string, extra map[string]string) (string, erro
 		"CoreSnapdBuildID":   coreBuildID,
 		"Date":               timeNow().Format(time.ANSIC),
 		"Snap":               snap,
-		"Channel":            channel,
 		"KernelVersion":      release.KernelVersion(),
 		"ErrorMessage":       errMsg,
-		"DuplicateSignature": fmt.Sprintf("snap-install: %s", errMsg),
+		"DuplicateSignature": dupSig,
 	}
 	for k, v := range extra {
 		// only set if empty
